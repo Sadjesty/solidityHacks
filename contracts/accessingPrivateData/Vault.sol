6742adcbc92395ee9ea9ff7b9eec309ff0e93997
Georgii Musikhin <sadjesty@gmail.com>
2021-04-22 07:00:07 +0300

    How to access private vars in EVM

---
+++ contracts/accessingPrivateData/Vault.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+
+contract Vault {
+
+    //slot 0
+    uint public count = 123;// 32 bytes (2**8) * 32
+    //slot 1
+    address public owner = msg.sender;// 20 bytes (2**8) * 20
+    bool public isTrue;// 1 byte
+    uint16 public u16 = 31;// 2 bytes(2**8) * 2
+    //slot 2
+    bytes32 private password;
+
+    //constants do not use storage (hardcoded in SC bytecode)
+    uint public constant SOME_CONST = 123;
+
+    //slot 3, 4, 5
+    bytes32[3] public data;
+
+    struct User {
+        uint id;
+        bytes32 password;
+    }
+
+    //slot 6 - length of the array
+    //starting from slot keccak256(6) - array elements
+    //slot where array element is stored = keccak256(slot) + (index * elementSize)
+    //where slot = 6 and elementSize = 2 (1 (uint) + 1 (bytes32))
+    User[] private users;
+
+    //slot 7 - empty
+    //entries are stored at keccak256(key, slot)
+    //where slot = 7, key = map key
+    mapping(uint => User) private idToUser;
+
+    constructor (bytes32 _password) {
+        password = _password;
+    }
+
+    function addUser(bytes32 _password) public {
+        User memory user = User({
+        id: users.length,
+        password: _password
+        });
+
+        users.push(user);
+        idToUser[user.id] = user;
+    }
+
+    function getArray(uint slot, uint index, uint elementSize) public pure returns(uint) {
+        return uint(keccak256(abi.encodePacked(slot))) + (index * elementSize);
+    }
+
+    function getMapLocation(uint slot, uint key) public pure returns (uint) {
+        return uint(keccak256(abi.encodePacked(key, slot)));
+    }
+}
